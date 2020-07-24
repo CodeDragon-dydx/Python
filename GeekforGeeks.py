dce4b6dc74b682005311208eb2099765659824fa
CodeDragon-dydx <54361813+CodeDragon-dydx@users.noreply.github.com>
2020-07-24 00:05:36 -0500

    Add files via upload

---
+++ GeekforGeeks.py
@@ -0,0 +1,43 @@
+
+# coding: utf-8
+
+# In[2]:
+
+print("Hello World")
+
+
+# In[3]:
+
+a = 3
+A = 4
+print(a)
+print(A)
+
+
+# In[5]:
+
+a = 3
+b = 9
+if b%a == 0:
+    print("b is divisible by a")
+elif b+1 == 10:
+    print("increment in b produces 10")
+else:
+    print("you are in else statement")
+
+
+# In[6]:
+
+def checkDivisibility(a,b):
+    if a%b == 0:
+        print("a is divisible by b")
+    else:
+        print("a is not divisible by b")
+    
+checkDivisibility(4,2)
+
+
+# In[ ]:
+
+
+
