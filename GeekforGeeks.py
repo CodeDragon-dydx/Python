c2c9cb1ea8c6a539dcb8e1f4b5621a96191016b0
CodeDragon-dydx <54361813+CodeDragon-dydx@users.noreply.github.com>
2020-07-28 23:40:20 -0500

    Add files via upload

--- GeekforGeeks.py
+++ GeekforGeeks.py
@@ -168,6 +168,20 @@ def fun(**kwargs):
 fun(name = "geeks", ID = "101", language = "Python")
 
 
+# In[5]:
+
+s = "10010"
+
+c = int(s,2)
+
+print("After converting to integer base 2:", end = "")
+print(c)
+
+e = float(s)
+print("After converting to float:", end = "")
+print(e)
+
+
 # In[ ]:
 
 
