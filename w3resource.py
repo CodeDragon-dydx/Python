e6db380b519e31a8e952985f4a10c6e9c09e63d3
CodeDragon-dydx <54361813+CodeDragon-dydx@users.noreply.github.com>
2020-08-05 22:52:20 -0500

    Add files via upload

---
+++ w3resource.py
@@ -0,0 +1,25 @@
+
+# coding: utf-8
+
+# In[1]:
+
+print("Twinkle, twinkle, little star, \n \t How I wonder what you are! \n \t \t Up above the world so high, \n \t \t Like a diamond in the sky. \n Twinkle, twinkle, little star, \n \t How I wonder what you are")
+
+
+# In[2]:
+
+import sys
+
+print("Python version")
+
+print(sys.version)
+
+print("Version Info")
+
+print(sys.version_info)
+
+
+# In[ ]:
+
+
+
