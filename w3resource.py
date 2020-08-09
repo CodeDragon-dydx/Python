5248d7eab2184667ff39b7140491550e655490c0
CodeDragon-dydx <54361813+CodeDragon-dydx@users.noreply.github.com>
2020-08-09 00:06:05 -0500

    Add files via upload

--- w3resource.py
+++ w3resource.py
@@ -19,6 +19,14 @@ print("Version Info")
 print(sys.version_info)
 
 
+# In[3]:
+
+import datetime
+now = datetime.datetime.now()
+print("Current date and time:")
+print(now.strftime("%Y-%m-%d %H:%M:%S"))
+
+
 # In[ ]:
 
 
