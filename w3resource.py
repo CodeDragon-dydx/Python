7b98cbeb5aa86fbf759cd61210e8900f866fe3d2
CodeDragon-dydx <54361813+CodeDragon-dydx@users.noreply.github.com>
2020-08-10 22:59:46 -0500

    Add files via upload

--- w3resource.py
+++ w3resource.py
@@ -27,6 +27,15 @@ print("Current date and time:")
 print(now.strftime("%Y-%m-%d %H:%M:%S"))
 
 
+# In[1]:
+
+from math import pi
+
+r = float(input("Input the radius of the circle:"))
+
+print("The area of the circle with radius"+str(r)+" is: "+str(pi*r**2))
+
+
 # In[ ]:
 
 
